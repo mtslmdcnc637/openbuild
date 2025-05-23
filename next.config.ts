f54fafd0c74ff4e2e8338e5d807a533f661153e5
Mateus lima <mtslmdcnc637@gmail.com>
2025-05-23 21:56:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://imageio.forbes.com/specials-images/imageserve/6656322068f31d3b932989cf/0x0.jpg?format=jpg&amp;crop=2437,1510,x275,y410,safe&amp;height=600&amp;width=1200&amp;fit=bounds) on `next/image`, hostname "imageio.forbes.com" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/components/editor/RenderedElement.tsx (119:9) @ RenderedElement
    >
    >   117 |         data-element-type={element.type}
    >   118 |       >
    > > 119 |         <Image
    >       |         ^
    >   120 |           src={element.attributes?.src || 'https://placehold.co/100x100.png'}
    >   121 |           alt={element.attributes?.alt || 'Imagem'}
    >   122 |           width={imageWidth || 100}
    >
    > Call Stack
    > 13
    >
    > Show 6 ignore-listed frame(s)
    > Array.map
    > <anonymous> (0:0)
    > RenderedElement
    > src/components/editor/RenderedElement.tsx (119:9)
    > <unknown>
    > src/components/editor/CanvasArea.tsx (102:13)
    > Array.map
    > <anonymous> (0:0)
    > CanvasArea
    > src/components/editor/CanvasArea.tsx (101:21)
    > EditorLayout
    > src/components/editor/EditorLayout.tsx (135:11)
    > CodeCanvasPage
    > src/app/page.tsx (9:7)

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'imageio.forbes.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
